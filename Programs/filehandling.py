63f537ed9d87fd1ceaf792ed1958d5f5d6d8baa8
Krushikk <krushikchandrashekar@gmail.com>
2021-12-19 02:29:34 +0530

    changes recovered

--- Programs/filehandling.py
+++ Programs/filehandling.py
@@ -7,7 +7,7 @@
 #         count += len(a)
 #     print(count)
 ########################################################################################################################
-# COUNT THE NUMBER OF LINES PRESENT IN A FILEe
+# COUNT THE NUMBER OF LINES PRESENT IN A FILE
 
 # f_path = r"C:\Users\hp\PycharmProjects\Python_Practice\files\samp.txt"
 # with open(f_path) as f:
--- Programs/krushikfile.py
+++
@@ -1,8 +0,0 @@
-# COUNT THE NUMBER OF LINES PRESENT IN A FILE
-
-# f_path = r"C:\Users\hp\PycharmProjects\Python_Practice\files\samp.txt"
-# with open(f_path) as f:
-#     count = 0
-#     for line in f:
-#         count += 1
-#     print(count)
